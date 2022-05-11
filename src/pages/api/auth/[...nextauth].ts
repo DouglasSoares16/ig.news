4948e009d313095d455468209e4239e1b1220fb5
Douglas Soares <douglasmaismcpe@gmail.com>
2022-05-11 19:18:25 -0300

    Verifying if user already exists

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
+import { fauna } from "../../../services/faunadb";
+import { query as q } from "faunadb";
+
 export default NextAuth({
   providers: [
     GitHubProvider({
@@ -12,5 +15,43 @@ export default NextAuth({
         }
       }
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection("users"),
+              {
+                data: {
+                  email
+                }
+              }
+            ),
+            q.Get(
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
 });
