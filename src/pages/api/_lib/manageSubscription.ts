bc073cd53a8cae1fc5dbddd36a438be6e641b62a
Douglas Soares <douglasmaismcpe@gmail.com>
2022-05-12 19:51:53 -0300

    Listening 'Checkout Session' Event of WebHook

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,38 @@
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/faunadb";
+import { stripe } from "../../../services/stripe";
+
+export async function saveSubscription(
+  subscription_id: string,
+  customer_id: string
+) {
+  const userRef = await fauna.query(
+    q.Select(
+      "ref",
+      q.Get(
+        q.Match(
+          q.Index("user_by_stripe_customer_id"),
+          customer_id
+        )
+      )
+    )
+  );
+
+  const subscription = await stripe.subscriptions.retrieve(subscription_id);
+
+  const subscriptionData = {
+    id: subscription.id,
+    user_id: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id,
+  };
+
+  await fauna.query(
+    q.Create(
+      q.Collection("subscriptions"),
+      {
+        data: subscriptionData
+      }
+    )
+  );
+}
