b51b154cca24e3f0de194502f0c898910f73e9d0
Douglas Soares <douglassoares16.dev@gmail.com>
2022-06-22 23:24:11 -0300

    Fix: Changed route of button subscribe

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -45,7 +45,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
 
           <div className={styles.continueReading}>
             Wanna continue reading?
-            <Link href="/home">
+            <Link href="/">
               <a> Subscribe now 😜</a>
             </Link>
           </div>
